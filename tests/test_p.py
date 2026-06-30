49d153bd48964da2aaf28351e5a04874241c786e
Chudinova Daria <xellbyy47@gmail.com>
2026-06-30 15:33:45 +0300

    Update test_p.py

--- tests/test_p.py
+++ tests/test_p.py
@@ -5,7 +5,7 @@ URL = 'https://api.pokemonbattle.ru/v2'
 TOKEN = '504534bd4560d697c23fafa6bf3387a1'
 HEADER = {'Content-Type':'application/json', 'trainer_token': TOKEN}
 TRAINER_ID = '4516'
-
+ 
 def test_status_code():
     response = requests.get(url = f'{URL}/pokemons', params = {'trainer_id' : TRAINER_ID}) 
     assert response.status_code == 200
